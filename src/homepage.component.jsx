ef81ac980fc0dcf4d8c6849b61abc6c1dd9735a5
abhiis <abhisheksrivastava295@gmail.com>
2019-12-27 22:13:21 +0530

    New repo

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -22,7 +22,7 @@ const HomePage = () =>(
         </div>
         <div className='menu-item'>
             <div className='content'>
-                <h1 className='title'>WOMEN</h1>
+                <h1 className='title'>WOMENS</h1>
                 <span className='subtitle'>SHOP NOW</span>
             </div>    
         </div>
