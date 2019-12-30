4d39feb41735f6a0a138d69de851e1603348629b
abhiis <abhisheksrivastava295@gmail.com>
2019-12-31 04:50:07 +0530

    Added Persist for localStorage of Cart, added directory reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,18 @@
 import {combineReducers} from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from './user/user.reducer';
 import CartReducer from './cart/cart.reducer';
-export default combineReducers({
+import DirectoryReducer from './directory/directory.reducer';
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+const rootReducer = combineReducers({
     user: userReducer,
-    cart: CartReducer
+    cart: CartReducer,
+    directory: DirectoryReducer
 });
+export default persistReducer(persistConfig, rootReducer);
+
